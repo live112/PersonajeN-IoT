b9fbe79b33ae1251a53a89c2d8c9e465558f453a
live112 <68666691+live112@users.noreply.github.com>
2023-12-12 22:03:59 -0600

    Add files via upload

---
+++ BluetoothBase/BluetoothBase.ino
@@ -0,0 +1,29 @@
+#include "BluetoothSerial.h"
+
+const char *pin = "1234";
+
+String device_name = "ESP32-BT-Slave";
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run make menuconfig to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin(device_name);
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+#ifdef USE_PIN
+  SerialBT.setPin(pin);
+  Serial.println("Using PIN");
+#endif
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+}
